7f0de401bce25f06b987303ff4739ccbf5437d2c
Otton Alves Teixeira <otton.alves@outlook.com>
2021-04-08 18:55:52 -0300

    Projeto de Estudo

---
+++ Exercicio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] values = Console.ReadLine().Split(' '); //Leitura da linha a matriz 
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.Write("Main diagonal: ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative number: " + count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExercicioLista/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int N = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Emplyoee #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Funcionario(id, nome, salario));
+            }
+            Console.WriteLine();
+            Console.WriteLine("Enter the employee id that will have salary increase: ");
+            int idSearch = int.Parse(Console.ReadLine());
+
+            Funcionario func = list.Find(x => x.Id == idSearch);
+            if (func != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                func.IncrementarSalario(percent);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            for (int i = 1; i <= N ; i++)
+            {
+                Console.WriteLine(func);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void IncrementarSalario(double percentual)
+        {
+            Salario += Salario * percentual / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
